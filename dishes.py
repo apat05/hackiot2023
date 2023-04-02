ed5f8fea6ce2e3c4ed422d4bde4097e8e71675b0
Quoc <qlnguyen@usc.edu>
2023-04-01 19:31:58 -0700

    Add files via upload
    Facial recognition docs

---
+++ dishes.py
@@ -0,0 +1,20 @@
+def add_dishes()->None:
+    #function will add dishes to the person whose face is recognized by the machine.
+
+def remove_dishes()->None:
+    #function will remove all the dishes of the person it detects
+def calculate_max()->:
+    #max dishes at end of day
+def person_detected():
+    #tells camera to take a pic when person is detected
+    #add dishes here too depending on if we're using the IR to detect the person or not
+    #if not using IR, then this function increments the person's dish count every time
+
+
+def main():
+
+
+
+if __name__=='__main__':
+    main()
+
