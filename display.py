8ff47d1c8e28547c2f8d6922c6c72cf1d5ddfb73
Russell Rodriguez <rar72397@usc.edu>
2023-04-01 19:46:04 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -30,20 +30,21 @@ def main():
     lcd_init()
 
     people = [0, 0, 0, 0]
-    default_range = ultrasonic()
+    default_range = 40
 
     while True:
         #outputting the string information to the screen
-        lcd_string("A: " + people[0] + " Q: " + people[1] + " K: " + people[2] + " R: " + people[3], 1)
+        lcd_byte(LCD_LINE_1, LCD_CMD)
+        lcd_string(f"A: {people[0]} Q: {people[1]} K: {people[2]} R: {people[3]}", 1)
         range = ultrasonic()
-        if(range < default_range):
+        if((range < default_range - 10) and (range > 10)):
             #use the camera, identify the person, and increment their counter
             id = 0 #numeric value returned by facial recognition software
 
             #increment the identified person's number
             people[id] += 1
         
-        while range != default_range: #so that the counter doesn't continue to update while person is putting dish in
+        while range < default_range - 10 and range > 0: #so that the counter doesn't continue to update while person is putting dish in
             range = ultrasonic()
             
             
@@ -70,7 +71,6 @@ def ultrasonic():
 
     return distance
 
-
 #!/usr/bin/python
 #
 # HD44780 LCD Test Script for
@@ -80,66 +80,6 @@ def ultrasonic():
 # Site   : http://www.raspberrypi-spy.co.uk
 # 
 # Date   : 03/08/2012
-#
-
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
-
-# def main():
-#   # Main program block
-
-#   # Initialise display
-#   lcd_init()
-
-#   # Toggle backlight on-off-on
-#   GPIO.output(LED_ON, True)
-#   time.sleep(1)
-#   GPIO.output(LED_ON, False)
-#   time.sleep(1)
-#   GPIO.output(LED_ON, True)
-#   time.sleep(1)
-
-#   # Send some centred test
-#   lcd_byte(LCD_LINE_1, LCD_CMD)
-#   lcd_string("Rasbperry Pi",2)
-#   lcd_byte(LCD_LINE_2, LCD_CMD)
-#   lcd_string("Model B",2)
-
-#   time.sleep(3) # 3 second delay
-
-#   # Send some left justified text
-#   lcd_byte(LCD_LINE_1, LCD_CMD)
-#   lcd_string("1234567890123456",1)
-#   lcd_byte(LCD_LINE_2, LCD_CMD)
-#   lcd_string("abcdefghijklmnop",1)
-
-#   time.sleep(3) # 3 second delay
-
-#   # Send some right justified text
-#   lcd_byte(LCD_LINE_1, LCD_CMD)
-#   lcd_string("Raspberrypi-spy",3)
-#   lcd_byte(LCD_LINE_2, LCD_CMD)
-#   lcd_string(".co.uk",3)
-
-#   time.sleep(30)
-
-#   # Turn off backlight
-#   GPIO.output(LED_ON, False)
 
 def lcd_init():
   GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
