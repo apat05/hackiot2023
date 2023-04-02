4cf83e6e0a9f05be9ba924053a85e89c93b9f1f9
Aum Patel <aumpatel@usc.edu>
2023-04-01 19:39:53 -0500

    Add files via upload

---
+++ display.py
@@ -0,0 +1,52 @@
+import lcd
+import face
+
+def main():
+
+    people = [0, 0, 0, 0]
+    default_range = ultrasonic()
+
+    while True:
+        #outputting the string information to the screen
+        lcd_string("A: " + people[0] + " Q: " + people[1] + " K: " + people[2] + " R: " + people[3], 1)
+        range = ultrasonic()
+        if(range < default_range):
+            #use the camera, identify the person, and increment their counter
+            id = #numeric value returned by facial recognition software
+
+            #increment the identified person's number
+            people[id] += 1
+        
+        while range != default_range: #so that the counter doesn't continue to update while person is putting dish in
+            range = ultrasonic()
+            
+            
+#Source: https://www.kitflix.com/how-to-interface-raspberry-pi-with-ultrasonic-sensor
+def ultrasonic():
+
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    GPIO.output(TRIG,False)
+    time.sleep(0.2)
+    GPIO.output(TRIG,True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG,False)
+    while GPIO.input(ECHO)==0:
+        pulse_start=time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end=time.time()
+    pulse_duration=pulse_end-pulse_start
+    distance=pulse_duration*17150
+    distance=round(distance,2)
+
+    return distance
+
+                
+
+
+                
+
+
+
+
+
